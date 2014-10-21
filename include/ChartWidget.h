bb39db7e67ae2e310ad8f7918dcb2c3f325b62ba
Sebastian Woetzel <wose@zuendmasse.de>
2014-10-21 20:50:19 +0200

    added empty chart widget

---
+++ include/ChartWidget.h
@@ -0,0 +1,20 @@
+#ifndef _EYETER_CHART_WIDGET_H_
+#define _EYETER_CHART_WIDGET_H_
+
+#include <QWidget>
+
+namespace EyeTER {
+    namespace ui {
+
+class ChartWidget : public QWidget
+{
+    Q_OBJECT
+
+ public:
+    ChartWidget(QWidget* parent = 0);
+};
+
+}
+}
+
+#endif
---
+++ src/ChartWidget.cpp
@@ -0,0 +1,13 @@
+#include <QtWidgets>
+
+#include "ChartWidget.h"
+
+namespace EyeTER {
+    namespace ui {
+
+ChartWidget::ChartWidget(QWidget* parent)
+{
+}
+
+}
+}
