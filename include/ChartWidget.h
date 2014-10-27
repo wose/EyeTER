a1fb904697b12dec3554b777f6e3ae5af74a6905
Sebastian Woetzel <wose@zuendmasse.de>
2014-10-27 21:01:38 +0100

    display crosshair, allow the user to move the bounds, some color tweaks

--- include/ChartWidget.h
+++ include/ChartWidget.h
@@ -20,15 +20,27 @@ class ChartWidget : public QWidget
     QSize sizeHint() const;
 
  protected:
+    void mouseMoveEvent(QMouseEvent* event);
+    void mousePressEvent(QMouseEvent* event);
+    void mouseReleaseEvent(QMouseEvent* event);
     void paintEvent(QPaintEvent* event);
 
  private:
     void drawDataSets(QPainter& painter);
     void drawLegend(QPainter& painter);
+    void drawCursor(QPainter& painter);
 
     std::vector<std::vector<long>> dataSets_;
     int lowerBound_;
     int upperBound_;
+    int cursorX_;
+    int cursorY_;
+
+    float scalingX_;
+    float scalingY_;
+
+    bool movingLowerBound_;
+    bool movingUpperBound_;
 
     static const int margin = 15;
     static const int tickLength = 5;
--- src/ChartWidget.cpp
+++ src/ChartWidget.cpp
@@ -1,5 +1,6 @@
 #include <algorithm>
 #include <stdlib.h>
+#include <iostream>
 #include <time.h>
 #include <vector>
 
@@ -12,9 +13,16 @@ namespace EyeTER {
 
 ChartWidget::ChartWidget(QWidget* parent) :
     QWidget(parent),
-    lowerBound_(20),
-    upperBound_(200)
+    lowerBound_(0),
+    upperBound_(256),
+    cursorX_(0),
+    cursorY_(0),
+    movingUpperBound_(false),
+    movingLowerBound_(false),
+    scalingX_(1.0),
+    scalingY_(1.0)
 {
+    setMouseTracking(true);
     srand (time(NULL));
 
     std::vector<long> dataSet;
@@ -22,6 +30,44 @@ ChartWidget::ChartWidget(QWidget* parent) :
         dataSet.push_back(rand() % 1000);
 
     dataSets_.push_back(dataSet);
+}
+
+void ChartWidget::mouseMoveEvent(QMouseEvent* event)
+{
+    cursorX_ = std::max(std::min(event->x(), width() - margin),
+                        static_cast<int>(margin));
+    cursorY_ = std::max(std::min(event->y(), height() - margin),
+                        static_cast<int>(margin));
+
+    if(movingLowerBound_)
+        lowerBound_ = (cursorX_ - margin) / scalingX_;
+    if(movingUpperBound_)
+        upperBound_ = (cursorX_ - margin) / scalingX_;
+
+    update();
+}
+
+void ChartWidget::mousePressEvent(QMouseEvent* event)
+{
+    if(event->button() == Qt::LeftButton) {
+        if(std::abs(cursorX_ - margin - lowerBound_ * scalingX_) <
+           std::abs(cursorX_ - margin - upperBound_ * scalingX_)) {
+            movingLowerBound_ = true;
+            lowerBound_ = (cursorX_ - margin) / scalingX_;
+        } else {
+            movingUpperBound_ = true;
+            upperBound_ = (cursorX_ - margin) / scalingX_;
+        }
+        update();
+    }
+}
+
+void ChartWidget::mouseReleaseEvent(QMouseEvent* event)
+{
+    if(event->button() == Qt::LeftButton) {
+        movingLowerBound_ = false;
+        movingUpperBound_ = false;
+    }
 }
 
 void ChartWidget::paintEvent(QPaintEvent* event)
@@ -33,13 +79,25 @@ void ChartWidget::paintEvent(QPaintEvent* event)
 
     drawDataSets(painter);
     drawLegend(painter);
+
+    if(!movingLowerBound_ && !movingUpperBound_)
+        drawCursor(painter);
+}
+
+void ChartWidget::drawCursor(QPainter& painter)
+{
+    painter.resetTransform();
+    QPen pen(Qt::black, 1, Qt::SolidLine);
+    painter.setPen(pen);
+
+    painter.drawLine(margin, cursorY_, width() - margin, cursorY_);
+    painter.drawLine(cursorX_, margin, cursorX_, height() - margin);
 }
 
 void ChartWidget::drawLegend(QPainter& painter)
 {
     painter.resetTransform();
     QPen pen(Qt::black, 1, Qt::SolidLine, Qt::FlatCap, Qt::MiterJoin);
-
     painter.setPen(pen);
     painter.drawLine(margin, margin, margin, height() - margin);
     painter.drawLine(margin, height() - margin,
@@ -69,61 +127,62 @@ void ChartWidget::drawDataSets(QPainter& painter)
 
     auto minmax = std::minmax_element(dataSet.begin(), dataSet.end());
 
-    float scalingX = static_cast<float>(width() - 2 * margin) / 256;
-    float scalingY = static_cast<float>(height() - 2 * margin) /
+    scalingX_ = static_cast<float>(width() - 2 * margin) / 256;
+    scalingY_ = static_cast<float>(height() - 2 * margin) /
         (*minmax.second - *minmax.first);
 
     QPainterPath outOfROI, inROI;
     for(int idx = 0; idx < lowerBound_; ++idx) {
-        outOfROI.addRect(scalingX * idx,
-                         -(dataSet[idx] - *minmax.first) * scalingY,
-                         scalingX, (dataSet[idx] - *minmax.first) * scalingY);
+        outOfROI.addRect(scalingX_ * idx,
+                         -(dataSet[idx] - *minmax.first) * scalingY_,
+                         scalingX_, (dataSet[idx] - *minmax.first) * scalingY_);
     }
 
     for(int idx = upperBound_; idx < dataSet.size(); ++idx) {
-        outOfROI.addRect(scalingX * idx,
-                         -(dataSet[idx] - *minmax.first) * scalingY,
-                         scalingX, (dataSet[idx] - *minmax.first) * scalingY);
+        outOfROI.addRect(scalingX_ * idx,
+                         -(dataSet[idx] - *minmax.first) * scalingY_,
+                         scalingX_, (dataSet[idx] - *minmax.first) * scalingY_);
 
     }
 
     for(int idx = lowerBound_; idx < upperBound_; ++idx) {
-        inROI.addRect(scalingX * idx, -(dataSet[idx] - *minmax.first) * scalingY,
-                      scalingX, (dataSet[idx] - *minmax.first) * scalingY);
+        inROI.addRect(scalingX_ * idx,
+                      -(dataSet[idx] - *minmax.first) * scalingY_,
+                      scalingX_, (dataSet[idx] - *minmax.first) * scalingY_);
     }
 
-    painter.fillPath(outOfROI, Qt::gray);
-    painter.fillPath(inROI, Qt::black);
+    painter.fillPath(outOfROI, Qt::darkGray);
+    painter.fillPath(inROI, Qt::darkBlue);
 
     // draw user bounds
     QPen pen(Qt::green, 1);
     painter.setPen(pen);
 
-    painter.drawLine(lowerBound_ * scalingX, 0,
-                     lowerBound_ * scalingX, -height() + 2*margin);
+    painter.drawLine(lowerBound_ * scalingX_, 0,
+                     lowerBound_ * scalingX_, -height() + 2*margin);
 
     QPainterPath lowerLabel;
-    lowerLabel.moveTo(lowerBound_ * scalingX, -height() + 2 * margin);
-    lowerLabel.lineTo(lowerBound_ * scalingX - 4, -height() + 2 * margin - 4);
-    lowerLabel.lineTo(lowerBound_ * scalingX + 4, -height() + 2 * margin - 4);
+    lowerLabel.moveTo(lowerBound_ * scalingX_, -height() + 2 * margin);
+    lowerLabel.lineTo(lowerBound_ * scalingX_ - 4, -height() + 2 * margin - 4);
+    lowerLabel.lineTo(lowerBound_ * scalingX_ + 4, -height() + 2 * margin - 4);
     painter.fillPath(lowerLabel, Qt::green);
 
-    painter.drawText(lowerBound_ * scalingX - 15, -height() + 2 * margin - 14,
+    painter.drawText(lowerBound_ * scalingX_ - 15, -height() + 2 * margin - 14,
                      30, 10,
                      Qt::AlignCenter, QString::number(lowerBound_));
 
     pen.setColor(Qt::red);
     painter.setPen(pen);
-    painter.drawLine(upperBound_ * scalingX, 0,
-                     upperBound_ * scalingX, -height() + 2*margin);
+    painter.drawLine(upperBound_ * scalingX_, 0,
+                     upperBound_ * scalingX_, -height() + 2*margin);
 
     QPainterPath upperLabel;
-    upperLabel.moveTo(upperBound_ * scalingX, -height() + 2 * margin);
-    upperLabel.lineTo(upperBound_ * scalingX - 4, -height() + 2 * margin - 4);
-    upperLabel.lineTo(upperBound_ * scalingX + 4, -height() + 2 * margin - 4);
+    upperLabel.moveTo(upperBound_ * scalingX_, -height() + 2 * margin);
+    upperLabel.lineTo(upperBound_ * scalingX_ - 4, -height() + 2 * margin - 4);
+    upperLabel.lineTo(upperBound_ * scalingX_ + 4, -height() + 2 * margin - 4);
     painter.fillPath(upperLabel, Qt::red);
 
-    painter.drawText(upperBound_ * scalingX - 15, -height() + 2 * margin - 14,
+    painter.drawText(upperBound_ * scalingX_ - 15, -height() + 2 * margin - 14,
                      30, 10,
                      Qt::AlignCenter, QString::number(upperBound_));
 
